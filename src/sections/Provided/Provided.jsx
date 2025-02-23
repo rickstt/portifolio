064b4959335a45641109dc2a16162540dc10d5aa
Henrique <rick@MacBook-Air-de-Henrique.local>
2025-02-22 23:52:45 -0300

    Feat: Services Provides Section with text in image

--- src/sections/Provided/Provided.jsx
+++ src/sections/Provided/Provided.jsx
@@ -1,7 +1,8 @@
 import React from 'react'
 import styles from './ProvidedStyles.module.css'
-import foto1 from '../../assets/img-coding.webp'
+import foto1 from '../../assets/img-dev.webp'
 import foto2 from '../../assets/img-meet.webp'
+import foto3 from '../../assets/img-seo.webp'
 import ProvidedCard from '../../common/ProvidedCard'
 
 function Provided() {
@@ -9,10 +10,18 @@ function Provided() {
     <section id='provided' className={styles.container}>
       <h1 className='sectionTitle'>Serviços Prestados</h1>
       <div className={styles.providedContainer}>
-        <ProvidedCard src={foto1} />
-        <ProvidedCard src={foto2} />
-        <ProvidedCard src={foto1} />
-        <ProvidedCard src={foto2} />
+        <div className={styles.overlay}>
+          <ProvidedCard src={foto1} />
+          <p className={styles.texto}>Criação sistemas, APIs, automações e aplicativos personalizados para tornar sua empresa mais eficiente e conectada.</p>
+        </div>
+        <div className={styles.overlay}>
+          <ProvidedCard src={foto2} />
+          <p className={styles.texto}>Consultoria e mentoria em tecnologia para ajudar sua empresa a implementar soluções digitais e tomar decisões estratégicas.</p>
+        </div>
+        <div className={styles.overlay}>
+          <ProvidedCard src={foto3} />
+          <p className={styles.texto}>Otimização de sites, sistemas e estratégias de marketing e SEO para melhorar sua presença online e aumentar a visibilidade.</p>
+        </div>
       </div>
     </section>
   )
